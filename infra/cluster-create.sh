c75b7d1db911397a1944859fa5287b9a9ae9abd1
shawon100 <shawonashadullah@gmail.com>
2022-09-03 14:36:51 +0600

    Added code base and eployments

---
+++ infra/cluster-create.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
+sudo mv -v minikube /usr/local/bin
+minikube version
+minikube start -p demo-cluster --nodes 3
